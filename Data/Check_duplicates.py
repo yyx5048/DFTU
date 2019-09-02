4220318b50308567f2fc730ef3a9fa1bd5955cfd
Yihuang Xiong <VincentXiong@Yihuangs-MacBook-Pro.local>
2019-09-02 15:35:08 -0400

    Committer: Yihuang Xiong <VincentXiong@Yihuangs-MacBook-Pro.local> 	Add check duplicates in the DMREF list:   Data/Check_duplicates.py 	Update the list by changing duplicate ones in to fake Sr100Ti200O300, order is maintained:   Data/Final_DMREF_Materials_List.csv 	Original csv file:   Data/Final_DMREF_Materials_List_Duplicate.csv

---
+++ Data/Check_duplicates.py
@@ -0,0 +1,28 @@
+import re
+import pandas as pd
+import numpy as np
+from collections import defaultdict
+
+df = pd.read_csv("Final_DMREF_Materials_List_Duplicate.csv")
+
+
+def list_duplicates(seq):
+    #--corresponding to the csv list idex with +2
+    tally = defaultdict(list)
+    for i, item in enumerate(seq):
+        tally[item].append(i)
+    return ((key,(np.array(locs)+2).tolist()) for key,locs in tally.items()
+                            if len(locs)>1)
+
+
+formula_list = []
+for idx,val in enumerate(df["Formula"].values):
+
+    #--split
+    tmp_form = re.findall('[A-Z][^A-Z]*', re.sub(" ", "" ,val))
+    tmp_form.sort()
+
+    formula_list.append("".join(tmp_form))
+
+for dup in list_duplicates(formula_list):
+    print(dup)
